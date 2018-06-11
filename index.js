8f90929ba1892b6e5bcef60e3ac5c46a0c4f1298
James M. Greene <james.m.greene@gmail.com>
2018-06-11 12:09:01 -0500

    Add support for native URL objects
    Fixes #2

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
+// Node.js core modules
+var querystring = require('querystring');
+
 // Userland modules
 var _ = require('lodash');
 var pickDeep = require('lodash-pickdeep');
 var keysDeep = require('deep-keys');
 
 
+var toString = Object.prototype.toString;
+
+// https://url.spec.whatwg.org/#url-miscellaneous
+function getDefaultPortFromScheme( scheme ) {
+  var port = null;
+  if ( scheme === 'ftp') {
+    port = 21;
+  }
+  else if ( scheme === 'gopher' ) {
+    port = 70;
+  }
+  else if ( scheme === 'http' || scheme === 'ws' ) {
+    port = 80;
+  }
+  else if ( scheme === 'https' || scheme === 'wss' ) {
+    port = 443;
+  }
+  return port;
+}
+
+function isWhatwgUrl( val ) {
+  return toString.call( val ) === '[object URL]';
+}
+
+function whatwgUrlToRecord( url ) {
+  var schemeLower = url.protocol.slice( 0, -1 ).toLowerCase();
+  return {
+    scheme: schemeLower,
+    username: url.username,
+    password: url.password,
+    hostname: url.hostname.toLowerCase(),
+    port: url.port || getDefaultPortFromScheme( schemeLower ),
+    path: url.pathname,
+    query: querystring.parse( ( url.search || '' ).slice( 1 ) ),
+    fragment: ( url.hash || '' ).slice( 1 )
+  };
+}
+
+function whatwgUrlComparator( val1, val2 ) {
+  var val1IsUrl = isWhatwgUrl( val1 );
+  var val2IsUrl = isWhatwgUrl( val2 );
+  if ( val1IsUrl || val2IsUrl ) {
+    if ( !( val1IsUrl && val2IsUrl ) ) {
+      return false;
+    }
+
+    if ( val1.href === val2.href ) {
+      return true;
+    }
+
+    return _.isEqual(
+      whatwgUrlToRecord( val1 ),
+      whatwgUrlToRecord( val2 )
+    );
+  }
+}
 
 module.exports = function( chai, util ) {
 
@@ -19,15 +78,23 @@ module.exports = function( chai, util ) {
 
       var isDeep = !!util.flag( this, 'deep' );
 
-      if ( isDeep && typeof actual === 'object' && typeof expected === 'object' && ( expected == null || Object.prototype.toString.call( expected ) !== '[object RegExp]' ) ) {
+      if ( isDeep && typeof actual === 'object' && typeof expected === 'object' && ( expected == null || toString.call( expected ) !== '[object RegExp]' ) ) {
         if ( msg ) {
           util.flag( this, 'message', msg );
         }
 
-        var matchResult =
-              ( actual == null && expected == null ) ||
-              _.isEqual( actual, expected ) ||
-              ( typeof actual === 'object' && typeof expected === 'object' && actual != null && expected != null && _.isMatch( actual, expected ) );
+        var matchResult = actual == null && expected == null;
+        if ( matchResult === false ) {
+          matchResult = whatwgUrlComparator( actual, expected );
+        }
+        // This particular `if` check is critical to correctly verifying URLs
+        if ( matchResult === undefined ) {
+          matchResult = _.isEqualWith( actual, expected, whatwgUrlComparator );
+
+          if ( matchResult === false ) {
+            matchResult = ( typeof actual === 'object' && typeof expected === 'object' && actual != null && expected != null && _.isMatchWith( actual, expected, whatwgUrlComparator ) );
+          }
+        }
 
         this.assert(
           matchResult,
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,14 @@
 /*global describe, context, it, chai, expect */
 
+// Node.js core modules
+var url = require('url');
+
 // Local modules
 var chaiDeepMatch = require('..');
 
+
+// Node.js >= 7.x
+var URL = global.URL || url.URL;
 
 var deepObj = {
       a: {
@@ -70,9 +76,146 @@ describe( 'chai-deep-match', function() {
       expect( deepObj ).to.deep.match( { e: 'baz' } );
     });
 
+    it( 'should not deeply match unequivalent subsets of objects', function() {
+      expect( deepObj ).to.not.deep.match( { e: 'foo' } );
+    });
+
     it( 'should accept a custom "message" argument', function() {
       expect( deepObj ).to.deep.match( { e: 'baz' }, 'My custom message' );
       expect( function() { expect( deepObj ).to.deep.match( { bad: 'nomatch' }, 'My custom error' ); } ).to.throw( Error, /My custom error/ );
+    });
+
+
+    // IMPORTANT:
+    // Skip all tests within this suite if the native URL type is not supported
+    ( !URL ? describe.skip : describe )( 'Node.js >= 7.x URLs', function() {
+
+      it( 'should work for basic URLs', function() {
+        var urlString = 'http://domain.com/';
+        var url1 = new URL( urlString );
+        var url2 = new URL( urlString );
+
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for ftp', function() {
+        var url1 = new URL( 'ftp://domain.com/' );
+        var url2 = new URL( 'ftp://domain.com:21/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for gopher', function() {
+        var url1 = new URL( 'gopher://domain.com/' );
+        var url2 = new URL( 'gopher://domain.com:70/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for http', function() {
+        var url1 = new URL( 'http://domain.com/' );
+        var url2 = new URL( 'http://domain.com:80/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for https', function() {
+        var url1 = new URL( 'https://domain.com/' );
+        var url2 = new URL( 'https://domain.com:443/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for ws', function() {
+        var url1 = new URL( 'ws://domain.com/' );
+        var url2 = new URL( 'ws://domain.com:80/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs on equivalent implicit ports for wss', function() {
+        var url1 = new URL( 'wss://domain.com/' );
+        var url2 = new URL( 'wss://domain.com:443/' );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for URLs with equivalent query strings', function() {
+        var urlString = 'http://domain.com/blah/';
+        var queryString1 = '?a=b&c=d';
+        var queryString2 = '?c=d&a=b';
+
+        var url1 = new URL( urlString + queryString1 );
+        var url2 = new URL( urlString + queryString1 );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+
+        url1 = new URL( urlString + queryString1 );
+        url2 = new URL( urlString + queryString2 );
+        expect( url1 ).to.deep.match( url2 );
+        expect( url2 ).to.deep.match( url1 );
+        expect( { key: url1 } ).to.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.deep.match( { key: url1 } );
+      });
+
+      it( 'should work for unequivalent URLs', function() {
+        var urlString = 'http://domain.com/blah/';
+        var urlQueryString = '?a=b&c=d';
+        var urlFragment = '#foo';
+
+        var url1 = new URL( urlString );
+        var url2 = new URL( urlString + urlQueryString );
+        var url3 = new URL( urlString + urlFragment );
+        var url4 = new URL( urlString + urlQueryString + urlFragment );
+
+        expect( url1 ).to.not.deep.match( url2 );
+        expect( url2 ).to.not.deep.match( url1 );
+        expect( { key: url1 } ).to.not.deep.match( { key: url2 } );
+        expect( { key: url2 } ).to.not.deep.match( { key: url1 } );
+
+        expect( url1 ).to.not.deep.match( url3 );
+        expect( url3 ).to.not.deep.match( url1 );
+        expect( { key: url1 } ).to.not.deep.match( { key: url3 } );
+        expect( { key: url3 } ).to.not.deep.match( { key: url1 } );
+
+        expect( url1 ).to.not.deep.match( url4 );
+        expect( url4 ).to.not.deep.match( url1 );
+        expect( { key: url1 } ).to.not.deep.match( { key: url4 } );
+        expect( { key: url4 } ).to.not.deep.match( { key: url1 } );
+
+        expect( url2 ).to.not.deep.match( url3 );
+        expect( url3 ).to.not.deep.match( url2 );
+        expect( { key: url2 } ).to.not.deep.match( { key: url3 } );
+        expect( { key: url3 } ).to.not.deep.match( { key: url2 } );
+
+        expect( url2 ).to.not.deep.match( url4 );
+        expect( url4 ).to.not.deep.match( url2 );
+        expect( { key: url2 } ).to.not.deep.match( { key: url4 } );
+        expect( { key: url4 } ).to.not.deep.match( { key: url2 } );
+
+        expect( url3 ).to.not.deep.match( url4 );
+        expect( url4 ).to.not.deep.match( url3 );
+        expect( { key: url3 } ).to.not.deep.match( { key: url4 } );
+        expect( { key: url4 } ).to.not.deep.match( { key: url3 } );
+      });
+
     });
 
   });
